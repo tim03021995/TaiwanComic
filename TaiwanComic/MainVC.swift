7a7f14e9171707964fd75aadeb41532f5c6410a0
tim960315@gmail.com <tim960315@gmail.com>
2020-11-24 18:29:11 +0800

    新增主頁面

---
+++ TaiwanComic/MainVC.swift
@@ -0,0 +1,13 @@
+//
+//  ContentView.swift
+//  TaiwanComic
+//
+//  Created by goodideas-studio on 2020/11/24.
+//
+import UIKit
+
+class MainVC: UIViewController {
+    override func loadView() {
+        self.view.backgroundColor = .mainColor
+    }
+}
